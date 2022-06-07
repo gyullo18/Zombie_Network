9410c85516dd0b5bb138bbf1974e32a7acd55721
gyullo18 <gold7098@naver.com>
2022-06-07 10:53:55 +0900

    20220607_1
    조음비 멀티

---
+++ Zombie/Assets/02.Scripts/LobbyManager.cs
@@ -0,0 +1,20 @@
+using Photon.Pun; // 유니티용 포톤 컴포넌트
+using Photon.Realtime; // 포톤 서비스 관련 라이브러리
+using UnityEngine;
+using UnityEngine.UI;
+
+// 마스터(매치메이킹) 서버와 룸 접속 담당
+public class LobbyManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
